f387f3d26a74f78cdf828d8f76017e9db69a2dec
WVinet <w.vinet.h@gmail.com>
2026-09-16 01:18:22 -0300

    feat(bff): assign CLIENT role by default

--- src/main/java/bff/security/JwtRoleConverter.java
+++ src/main/java/bff/security/JwtRoleConverter.java
@@ -2,35 +2,33 @@ package bff.security;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class JwtRoleConverter
-        implements Converter<Jwt, AbstractAuthenticationToken> {
+public class JwtRoleConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
 
         List<String> roles = jwt.getClaimAsStringList("roles");
 
-        Collection<GrantedAuthority> authorities =
-                roles == null
-                        ? Collections.emptyList()
-                        : roles.stream()
-                          .map(role -> (GrantedAuthority)
-                               new SimpleGrantedAuthority(
-                                       "ROLE_" + role
-                               )
-                          )
-                          .toList();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        if (roles == null || roles.isEmpty()) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_CLIENT"));
+        } else {
+            roles.forEach(role ->
+                    authorities.add(
+                            new SimpleGrantedAuthority("ROLE_" + role)
+                    )
+            );
+        }
 
         return new JwtAuthenticationToken(
                 jwt,
